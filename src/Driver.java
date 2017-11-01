f1ca014f023379fa2e549828ce49cee9fe50d7c3
Ben Andrews <ben13100@msn.com>
2017-10-31 19:59:44 -0500

    Added arrival time and priority to the process object.

--- src/Driver.java
+++ src/Driver.java
@@ -21,6 +21,5 @@ public class Driver
 		
 		// build process queue
 		
-		// 
 	}
 }
--- src/Process.java
+++ src/Process.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 public class Process 
 {
 	public int number = 0;
+	int arrivalTime = 0;
+	int priority = 0; // between 1 and 99
 	public int memory = 0;
 	public Queue<Burst> bursts = new LinkedList<Burst>();	
 	
